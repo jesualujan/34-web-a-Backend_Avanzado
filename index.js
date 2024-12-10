1d56c9f78b2fbf61f197d5fda2f469ed3d876000
jesualujan <lujan.oficial@gmail.com>
2024-12-09 19:37:34 -0600

    ✨mejoramos el server

--- index.js
+++ index.js
@@ -1,13 +1,19 @@
 //* Crear un servidor usando express server
 const express = require('express');
+const colores = require('colors')
+// cargar las variables de entorno desde el archivo .env
+// require('dotenv').config();
+
+//configuración del puerto
+const port = process.env.PORT || 3000;
 
 const app = express();
 
 //ruta raiz
 app.get('/' , (req,res) =>{
-    res.send('Servidor Funcionando')
+    res.send('servidor corriendo');
 })
 
-app.listen(3000, () => {
-    console.log('Server on port 3000');
+app.listen(port, () => {
+    console.log('Servidor corriendo en: 🚀',`http://localhost:${port}`.blue.bold );
 });
