11e61389bf79fcb97d65c7b2dc1069606c692bdb
jesualujan <lujan.oficial@gmail.com>
2024-12-09 21:23:38 -0600

    ✨ importamos la conexión a la base de datos

--- index.js
+++ index.js
@@ -1,44 +1,49 @@
 //* Crear un servidor usando express server
-const express = require('express');
-const morgan = require('morgan');
-const colores = require('colors')
+import express from 'express'
+import morgan from 'morgan'
+import connect from './utils/db.js'
+import colors from 'colors'
+
 // cargar las variables de entorno desde el archivo .env
 // require('dotenv').config();
-const app = express();
+const app = express()
 
-//configuración del puerto
-const port = process.env.PORT || 3000;
+// configuración del puerto
+const port = process.env.PORT || 3000
 
-// configuración de middlewares 
+// configuración de middlewares
 // middlware para registrar las solicitiudes HTTP en la consola
 app.use(morgan('combined'))
 // middlware para analizar el cuerpo de las solicitudes en formato JSON
 app.use(express.json())
 // middlware para analizar el cuerpo de las solicitudes con datos codificados en URL
-app.use(express.urlencoded({extended: true}))
+app.use(express.urlencoded({ extended: true }))
 
-//middlware para manejar errores 
-app.use((err, req, res, next ) => {
-    console.log(err.stack)
-    res.status(500).send('Something broke!'.red.bold)
+// middlware para manejar errores
+app.use((err, req, res, next) => {
+  console.log(err.stack)
+  res.status(500).send('Something broke!'.red.bold)
 })
 
-//ruta raiz
-app.get('/' , (req,res) =>{
-    res.send('servidor corriendo');
+// ruta raiz
+app.get('/', (req, res) => {
+  res.send('servidor corriendo')
 })
 
-app.get('/api', (req,res) =>
-     { res.send({ 
-        message: 'Hola mundo desde express',
-        author: 'Johan',
-        lastname: 'Gonzalez',
-        age: 30,
-        email: 'johan@gmail.com',
-        phone: '123456789',
-    })
+app.get('/api', (req, res) => {
+  res.send({
+    message: 'Hola mundo desde express',
+    author: 'Johan',
+    lastname: 'Gonzalez',
+    age: 30,
+    email: 'johan@gmail.com',
+    phone: '123456789'
+  })
 })
 
 app.listen(port, () => {
-    console.log('Servidor corriendo en: 🚀',`http://localhost:${port}`.blue.bold );
-});
+  console.log('Servidor corriendo en: 🚀', `http://localhost:${port}`.blue.bold)
+})
+
+// conectar a la base de datos
+connect()
